ec0474aaa19275b4fa54124c194e8d59fe9e7564
KiichiYuumi <kiichiyuumi0000@gmail.com>
2026-07-14 16:15:56 +0900

    a

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
   "use strict";
-  console.log('enabled');
-
   //id tate scroll
 const boxes = document.querySelectorAll('.tate');
 
 boxes.forEach((box) => {
+  box.addEventListener('wheel', (e) => {
+    e.preventDefault();
+    box.scrollLeft -= e.deltaY;
+  }, { passive: false });
+});
+
+  //id tateNB scroll
+const boxesNB = document.querySelectorAll('.tateNB');
+
+boxesNB.forEach((box) => {
   box.addEventListener('wheel', (e) => {
     e.preventDefault();
     box.scrollLeft -= e.deltaY;
